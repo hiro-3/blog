8b0bc4df744307e5d3bb49fa7ac4b872b074931a
Hirotoshi <hiro.chibita@gmail.com>
2020-05-27 04:50:12 +0900

    article2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,48 @@
 class ArticlesController < ApplicationController
-
+  before_action :article_find, only:[:show,:edit,:update,:destroy]
   def index
     #binding.pry
     @articles = Article.order(created_at: :desc)
+  end
+
+  def show
   end
 
   def new
     @articles = Article.new
   end
 
+  def create
+    @articles = Articles.new(article_params)
+    if @articles.save
+      redirect_to @articles, notice:"作成完了"
+    else
+      render :new, alert:"作成失敗"
+  end
+
+  def edit
+  end
+
+  def update
+    if @articles.update(article_params)
+      redirect_to @article, notice:"更新完了"
+    else
+      render :edit, alert:"更新失敗"
+  end
+
+  def destroy
+    if @articles.destroy
+      redirect_to root_path, notice:"削除完了"
+    else
+      render  root_path, alrert:"削除失敗"
+  end
+
+  private
+    def article_find
+     @articles = Article.find(params[:id])
+    end
+
+    def article_params
+       params.require(:article).permit(:title,:content)
+    end
 end
